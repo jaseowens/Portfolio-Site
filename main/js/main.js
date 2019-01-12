1318334186e617056f6cc996390e2ac1959c1fab
Jase Owens <jaseowens197@gmail.com>
2019-01-12 16:40:43 -0600

    Design gallery implemented
    Desing gallery has been made, cards are functional, need to make cards design better

---
+++ main/js/main.js
@@ -0,0 +1,15 @@
+window.onload = init();
+
+// function init(){
+//     // populateDesignGallery();
+// }
+
+// function populateDesignGallery() {
+//     var gallery = document.getElementById('gallery');
+//     for(var i = 0; i<designImages; i++){
+//         let curImg = document.createElement('div');
+//         curImg.classList.add('gallery-img');
+//         curImg.style.backgroundImage();
+//         gallery.appendChild(curImg);
+//     }
+// }
