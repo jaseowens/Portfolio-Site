95ba20f60ea94bb0bca4aa6c8e6af4e9fa864634
jaseowens <jowens@uab.edu>
2019-01-14 17:45:17 -0600

    pop in on scroll
    Found a neat little trick online, put in as proof of concept, want to do something similar, but more noticable.

--- main/js/main.js
+++ main/js/main.js
@@ -8,4 +8,70 @@ function init(){
         $('html,body').unbind().animate({scrollTop: $("#design").offset().top-160},'fast');
     });
 
+
+    (function($) {
+
+        /**
+         * 
+         * FOUND AT: https://css-tricks.com/slide-in-as-you-scroll-down-boxes/
+         * 
+         * Copyright 2012, Digital Fusion
+         * Licensed under the MIT license.
+         * http://teamdf.com/jquery-plugins/license/
+         *
+         * @author Sam Sehnert
+         * @desc A small plugin that checks whether elements are within
+         *     the user visible viewport of a web browser.
+         *     only accounts for vertical position, not horizontal.
+         */
+      
+        $.fn.visible = function(partial) {
+          
+            var $t            = $(this),
+                $w            = $(window),
+                viewTop       = $w.scrollTop(),
+                viewBottom    = viewTop + $w.height(),
+                _top          = $t.offset().top,
+                _bottom       = _top + $t.height(),
+                compareTop    = partial === true ? _bottom : _top,
+                compareBottom = partial === true ? _top : _bottom;
+          
+          return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+      
+        };
+          
+      })(jQuery);
+
+      $(window).scroll(function(event) {
+  
+        $(".module").each(function(i, el) {
+          var el = $(el);
+          if (el.visible(true)) {
+            el.addClass("come-in"); 
+          } 
+        });
+        
+      });
+
+      var win = $(window);
+        var allMods = $(".design-gallery-card");
+
+        // Already visible modules
+        allMods.each(function(i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            el.addClass("already-visible"); 
+        } 
+        });
+
+        win.scroll(function(event) {
+        
+        allMods.each(function(i, el) {
+            var el = $(el);
+            if (el.visible(true)) {
+            el.addClass("come-in"); 
+            } 
+        });
+        
+        });
  }
