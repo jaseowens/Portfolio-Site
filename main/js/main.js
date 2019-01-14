85ef7352cc1066bb9321b7deee1a858e30401248
jowens <jowens@ANES-JT-965C-F>
2019-01-14 17:40:18 -0600

    Design updates, jQuery inclusion, nav functioning
    jQuery included in project,
    nav moved to top right of screen,
    design button works,
    gallery has been offset to give overlap into next section

--- main/js/main.js
+++ main/js/main.js
@@ -1,15 +1,11 @@
 window.onload = init();
 
-// function init(){
-//     // populateDesignGallery();
-// }
 
-// function populateDesignGallery() {
-//     var gallery = document.getElementById('gallery');
-//     for(var i = 0; i<designImages; i++){
-//         let curImg = document.createElement('div');
-//         curImg.classList.add('gallery-img');
-//         curImg.style.backgroundImage();
-//         gallery.appendChild(curImg);
-//     }
-// }
+
+
+function init(){
+    $("#design-button").click( function() {
+        $('html,body').unbind().animate({scrollTop: $("#design").offset().top-160},'fast');
+    });
+
+ }
